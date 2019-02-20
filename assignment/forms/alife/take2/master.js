a492fef126c7efe1e329eeb82e3a934e1d9f04e7
alife4135 <letto4135@gmail.com>
2019-02-20 18:05:03 -0500

    finished?..

--- assignment/forms/alife/take2/master.js
+++ assignment/forms/alife/take2/master.js
@@ -5,6 +5,7 @@ var email;
 var passwordInput = document.getElementById("passwordInput");
 var x = document.getElementById("message");
 var passwordConfirm;
+var newUser = [];
 
 passwordInput.onfocus = function() {
     x.style.opacity = "1";
@@ -46,7 +47,7 @@ submitMe.addEventListener('click', () => {
             return false;
         }
         // check for email
-        if(email == ""){
+        if(email == "" || ! email.match("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")){
             alert("Please enter an email.");
             return false;
         }
@@ -56,22 +57,51 @@ submitMe.addEventListener('click', () => {
             return false;
         }
         // if passwords and that they match
-        if(passwordConfirm == "" || password == ""){
-            alert("Please make sure you entered a password and confirmed it.");
+        if(passwordConfirm == "" || password == "" || ! password.match("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{2,}$")){
+            alert("Please make sure you entered a valid password and confirmed it.");
             return false;
         }else if(passwordConfirm != password){
             alert("Please make sure passwords match.");
             return false;
         }
+
+        // Display results when conditions are met.
+        var user = {
+            userFirstName: firstName,
+            userLastName: lastName,
+            userEmail: email,
+            userUsername: userName,
+            userPassword: password
+        }
+        newUser.push(user);
+        console.log(newUser);
+        var userMap = newUser.map(x => `<div class="col-sm-3">
+                                            <div class="card">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">${x.userUsername}</h5>
+                                                    <h6 class="card-subtitle mb-2 text-muted">${x.userEmail}</h6>
+                                                    <p class="card-text">${x.userFirstName}</p>
+                                                    <p class="card-text">${x.userLastName}</p>
+                                                    <p class="card-text">${x.userPassword}</p>
+                                                </div>
+                                            </div>
+                                        </div>`);
+        var userDisplayArea = document.getElementById("showNewUsers");
+        var display = userMap.join();
+        userDisplayArea.innerHTML = display;
     }else{
         // on index page
         // get values from inputs
         email = document.getElementById("emailInput").value;
         password = document.getElementById("passwordInput").value;
-        // if password matches regex alert success and show email and password.
-        if(password.match("^[a-zA-Z0-9]\\w{2,14}$")){
-            alert("Huzzah, you're a genius!\n Email: " + email + "\nPassword: " + password);
-        }
+        // if checkbox is checked, password matches regex, and email not blank alert success and show email and password.
+        if(document.getElementById("rememberMe").checked == true){
+            if(password.match("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{2,}$") && email.match("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")){
+                alert("Huzzah, you're a genius!\n Email: " + email + "\nPassword: " + password);
+            }else{
+                alert("You must've screwed something up. Good job.");
+            }
+    }
         // also logs email and password to console.
         console.log(email + " " + password);
     }
